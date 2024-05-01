b5e74c1c475c604f44750df8ed05ef37b408383c
AlSatan666 <145715865+AlSatan666@users.noreply.github.com>
2024-05-01 11:39:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    darkToggle = document.querySelector(".dark-light"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    sidebarClose = document.querySelector(".sidebarClose");
 
+    let getMode = localStorage.getItem("mode"); /* serve a mantenere il dark/light theme anche se refresho*/
+    if (getMode && getMode=="dark-mode") {
+            body.classList.add("dark");
+    }
+    
+
+    darkToggle.addEventListener("click", () =>{
+        darkToggle.classList.toggle("active"); /* alterna i bottoni dark/light*/
+        body.classList.toggle("dark"); /* aggiunge la classe dark al body sul click*/ 
+        if (!body.classList.contains("dark")) {
+            localStorage.setItem("mode", "light-mode");
+        } else {
+            localStorage.setItem("mode", "dark-mode")
+        }
+    })
+
+
+
+sidebarOpen.addEventListener("click", () =>{
+    nav.classList.add("active");
+})
+
+body.addEventListener("click", e =>{
+    let clicked = e.target;
+
+    if (!clicked.classList.contains("sidebarOpen") && !clicked.classList.contains("menu")) {
+        nav.classList.remove("active");
+    }
+})
+
+
+// Gestione apertura e chiusura finestra modale per il login
+document.addEventListener('DOMContentLoaded', function () {
+    const loginBtn = document.getElementById('loginBtn');
+    const loginModal = document.getElementById('loginModal');
+    const closeModal = document.querySelector('.close');
+
+    loginBtn.addEventListener('click', function () {
+        loginModal.style.display = 'block';
+    });
+
+    closeModal.addEventListener('click', function () {
+        loginModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (e) {
+        if (e.target == loginModal) {
+            loginModal.style.display = 'none';
+        }
+    });
+});
